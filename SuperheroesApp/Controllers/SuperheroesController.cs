7e117bffb473e34e32bb083439e6882317a4a87c
Cody <cemt5462@gmail.com>
2023-07-07 16:12:26 -0500

    Created and linked db with superheroes model + initialized superheroes controller + created first index view.

---
+++ SuperheroesApp/Controllers/SuperheroesController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperheroesApp.Models;
+
+namespace SuperheroesApp.Controllers
+{
+    public class SuperheroesController : Controller
+    {
+        // GET: SuperheroesController
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        // GET: SuperheroesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperheroesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperheroesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
